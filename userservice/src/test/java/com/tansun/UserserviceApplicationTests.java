e36f9bc934665403af379ed4cdad876522a72e69
wuhuai <whsir_1014@163.com>
2021-06-09 10:12:20 +0800

    修改 springcloud 版本，让 bootstrap 生效，测试端点refresh刷新

--- userservice/src/test/java/com/tansun/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/tansun/UserserviceApplicationTests.java
@@ -1,13 +1,20 @@
 package com.tansun;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
 class UserserviceApplicationTests {
 
+    @Value("${sp.user-service.users}")
+    private String userJson;
     @Test
     void contextLoads() {
+        System.out.println("userJson--->"+userJson);
     }
 
 }
